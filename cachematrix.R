177e1ea749ba2b2d0c936822f975355916713177
Sebastien Leuba <sebastien007@users.noreply.github.com>
2015-05-24 23:16:53 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,16 @@
 ## Put comments here that give an overall description of what your
-## functions do
-
-
+## Create function , mycachematrix
 myCacheMatrix <- function(x = matrix()) {
-  ##              1. set the matrix
-  ##              2. get the matrix
-
-  ##         this list is used as the input to cacheSolve()
   
   inv = NULL
   set = function(y) {
-    # use `<<-` to assign a value to an object in an environment 
-    # different from the current environment. 
     x <<- y
     inv <<- NULL
   }
   get = function() x
-  setinv = function(inverse) inv <<- inverse 
-  getinv = function() inv
+  setinv = function(inverse) inv <<- inverse ## set inverce of the matrix 
+  getinv = function() inv  ## 
   list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
+
 
